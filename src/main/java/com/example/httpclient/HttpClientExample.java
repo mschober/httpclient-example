feaf44eb73494ac4b0a3729e8cb0fc743768a124
Mike Schober <mschober@cj.com>
2012-09-17 20:04:04 -0700

    updating pom to use httpclient deps

---
+++ src/main/java/com/example/httpclient/HttpClientExample.java
@@ -0,0 +1,14 @@
+package com.example.httpclient;
+
+import org.apache.commons.httpclient.*;
+import org.apache.commons.httpclient.methods.*;
+import org.apache.commons.httpclient.params.HttpMethodParams;
+
+import java.io.*;
+
+public class HttpClientExample {
+
+    public static void main( String[] args ) {
+        HttpClient client = new HttpClient();
+    }
+}
