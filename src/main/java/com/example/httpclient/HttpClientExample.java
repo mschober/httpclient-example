15faf708ed7738feb41c40ee8590bad3a25cc999
Mike Schober <mschober@cj.com>
2012-09-17 20:22:04 -0700

    figured out how to use jade to get uber jar going

--- src/main/java/com/example/httpclient/HttpClientExample.java
+++ src/main/java/com/example/httpclient/HttpClientExample.java
@@ -10,5 +10,15 @@ public class HttpClientExample {
 
     public static void main( String[] args ) {
         HttpClient client = new HttpClient();
+        HttpMethod method = new GetMethod("http://www.apache.org/");
+
+        try {
+            client.executeMethod(method);
+            byte[] responseBody = method.getResponseBody();
+            System.out.println(new String(responseBody));
+        } catch(IOException e) {
+            //fail
+        }
+
     }
 }
